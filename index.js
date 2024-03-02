67917802e1cdc20fa91dd7c52984e8790b3ea7d4
Alkaend <Eyvaz_fi99@itstep.edu.az>
2024-03-02 13:47:14 +0400

    hw-30-11-task1 sdan

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-function findProto (object) {
-    do {
-        object = Object.getPrototypeOf(object);
-        console.log(object);
-      } while (object);
+let links = document.querySelectorAll('a');
+
+function getRandomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
 
-findProto(new Date());
+for (let link of links) {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+
+    });
+    link.addEventListener('mouseover', (e) => { link.style.color = getRandomColor(); });
+}
+
